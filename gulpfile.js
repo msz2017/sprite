3ab1cec34e2939210893eb7afb72257dc259e50c
shuz <15558057209@163.com>
2018-07-27 13:00:56 +0800

    image

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,21 @@ var merge = require('merge-stream');
 
 var spritesmith = require('gulp.spritesmith');
 
-var gulp = require('gulp');
-var spritesmith = require('gulp.spritesmith');
-
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('images/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
+  var spriteData = gulp.src('h5/*.png').pipe(spritesmith({
+    imgName: 'sprite-h5.png',
     cssName: 'sprite.css',
+    padding: 10,
     cssTemplate: function (data) {
       var arr = [];
       data.sprites.forEach(function (sprite) {
         arr.push(`
         .icon-${sprite.name} {
           background-image: image_url("${sprite.escaped_image}");
-          background-position: ${sprite.offset_x / 2}px ${sprite.offset_y / 2}px;
-          background-size: ${sprite.total_width / 2}px ${sprite.total_height / 2}px;
-          width: ${sprite.width / 2}px;
-          height: ${sprite.height / 2}px;
+          background-position: ${Math.round(sprite.offset_x / 2)}px ${Math.round(sprite.offset_y / 2)}px;
+          background-size: ${Math.round(sprite.total_width / 2)}px ${Math.round(sprite.total_height / 2)}px;
+          width: ${Math.round(sprite.width / 2)}px;
+          height: ${Math.round(sprite.height / 2)}px;
         }
         `.trim())
       });
