1927fb0a985b42d6230d0fee4ef0be4719163e2d
shuz <15558057209@163.com>
2018-08-14 14:16:41 +0800

    all

--- gulpfile.js
+++ gulpfile.js
@@ -10,22 +10,10 @@ gulp.task('sprite', function () {
   var spriteData = gulp.src('huopu/*.png').pipe(spritesmith({
     imgName: 'huopu.png',
     cssName: 'sprite.css',
-    padding: 2,
-    cssTemplate: function (data) {
-      var arr = [];
-      data.sprites.forEach(function (sprite) {
-        arr.push(
-`
-.icon-${sprite.name} {
-  background-image: image_url("${sprite.escaped_image}");
-  background-position: ${Math.round(sprite.offset_x / 2)}px ${Math.round(sprite.offset_y / 2)}px;
-  background-size: ${Math.round(sprite.total_width / 2)}px ${Math.round(sprite.total_height / 2)}px;
-  width: ${Math.round(sprite.width / 2)}px;
-  height: ${Math.round(sprite.height / 2)}px;
-}`)
-      });
-
-      return arr.join('');
+    padding: 4,
+    cssTemplate: 'handlebarsStr.css.handlebars',
+    cssHandlebarsHelpers: {
+      half: function (num) { return `${num / 2}px`; }
     }
   }));
   return spriteData.pipe(gulp.dest('output/'));
